15e1956ce35dc8125fde5355fe4a6240cd2b0c26
danielbill <danielbill95@gmail.com>
2020-12-26 23:38:39 +0100

    added thymeleaf structure

--- src/main/java/bil0104/vea/Controllers/Web/StudentController.java
+++ src/main/java/bil0104/vea/Controllers/Web/StudentController.java
@@ -2,18 +2,26 @@ package bil0104.vea.Controllers.Web;
 
 import bil0104.vea.Services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Locale;
+
 @Controller
 public class StudentController {
+    @Autowired
+    private MessageSource messageSource;
+
     @Autowired
     private StudentService studentService;
 
     @GetMapping("/students")
     public String list(Model model) {
         model.addAttribute("students", studentService.getAll());
+        model.addAttribute("metaTitle", messageSource.getMessage("Students.Head.Title", null, LocaleContextHolder.getLocale()));
         return "views/students/list";
     }
 
--- src/main/java/bil0104/vea/Controllers/Web/SubjectController.java
+++ src/main/java/bil0104/vea/Controllers/Web/SubjectController.java
@@ -2,6 +2,8 @@ package bil0104.vea.Controllers.Web;
 
 import bil0104.vea.Services.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +11,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SubjectController {
     @Autowired
+    private MessageSource messageSource;
+
+    @Autowired
     private SubjectService subjectService;
 
     @GetMapping("/subjects")
     public String list(Model model) {
         model.addAttribute("subjects", subjectService.getAll());
+        model.addAttribute("metaTitle", messageSource.getMessage("Subjects.Head.Title", null, LocaleContextHolder.getLocale()));
         return "views/subjects/list";
     }
 
