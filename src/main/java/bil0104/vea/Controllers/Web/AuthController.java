8222f9ffed7e42b39235606cf5fe9deef74c0694
Kechis1 <danielbill95@gmail.com>
2020-12-29 23:03:52 +0100

    Added sec:auth to header html

--- src/main/java/bil0104/vea/Controllers/Web/AuthController.java
+++ src/main/java/bil0104/vea/Controllers/Web/AuthController.java
@@ -3,6 +3,10 @@ package bil0104.vea.Controllers.Web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +18,10 @@ public class AuthController {
 
     @GetMapping("/login")
     public String login(Model model) {
+        if (isAuthenticated()) {
+            return "redirect:/";
+        }
+        model.addAttribute("pageActive", "login");
         model.addAttribute("metaTitle", messageSource.getMessage("Home.Login.Body.Title", null, LocaleContextHolder.getLocale()));
         return "views/home/login";
     }
@@ -22,5 +30,14 @@ public class AuthController {
     public String error(Model model) {
         model.addAttribute("metaTitle", messageSource.getMessage("Errors.NotAllowed", null, LocaleContextHolder.getLocale()));
         return "views/errors/403";
+    }
+
+    private boolean isAuthenticated() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || AnonymousAuthenticationToken.class.
+                isAssignableFrom(authentication.getClass())) {
+            return false;
+        }
+        return authentication.isAuthenticated();
     }
 }
