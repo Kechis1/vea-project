1585fa221353174123eb5a52e13c4a8708638dfe
Kechis1 <danielbill95@gmail.com>
2021-01-03 15:25:13 +0100

    finished person api

--- src/main/java/bil0104/vea/REST/PersonRestController.java
+++ src/main/java/bil0104/vea/REST/PersonRestController.java
@@ -1,12 +1,15 @@
 package bil0104.vea.REST;
 
 import bil0104.vea.Entities.Person;
+import bil0104.vea.Entities.Role;
 import bil0104.vea.Services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static bil0104.vea.Utils.EncryptedPasswordUtils.encryptPassword;
 
 @RestController
 public class PersonRestController {
@@ -25,6 +28,9 @@ public class PersonRestController {
 
     @RequestMapping(path = "/api/persons", method = RequestMethod.POST)
     public Person create(@Validated @RequestBody Person person) {
+        person.setLogin(Person.findNextLogin(person, personService::findByLogin));
+        person.setRole(Role.ADMIN);
+        person.setPassword(encryptPassword(person.getPassword()));
         personService.insert(person);
         return null;
     }
